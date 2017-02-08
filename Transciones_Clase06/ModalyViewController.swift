8cbe085251f8d49719a6f9b19a0193db573fc2d4
jose luis yana <joseyananicolas@gmail.com>
2017-02-07 21:12:46 -0500

    Transiciones, clase número 06

---
+++ Transciones_Clase06/ModalyViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ModalyViewController.swift
+//  Transciones_Clase06
+//
+//  Created by alumno on 7/02/17.
+//  Copyright © 2017 alumno. All rights reserved.
+//
+
+import UIKit
+
+class ModalyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnReturnTouch(_ sender: UIButton) {
+        
+        self.dismiss(animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
